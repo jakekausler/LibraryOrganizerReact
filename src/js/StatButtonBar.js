82468c55901736f64fca3c6e80da8b537ae26454
jakekausler <jakekausler@yahoo.com>
2019-10-10 16:19:10 -0400

    Start on button nav bar

--- src/js/StatButtonBar.js
+++ src/js/StatButtonBar.js
@@ -5,10 +5,34 @@ class StatButtonBar extends React.Component {
 		super()
 	}
 
+	//TODO: Change active on click
 	render() {
 		return (
-			<div>
-			Button Bar
+			<div className="NavBar">
+				<button className="active" onClick={() => this.props.ChangeStat('generalbycounts')}>General</button>
+				<div className="dropdown">
+					<button className="dropbtn">Deweys</button>
+					<div className="dropdown-content">
+						<button onClick={() => this.props.ChangeStat('')}>Owned</button>
+						<button onClick={() => this.props.ChangeStat('')}>Wishlist</button>
+						<button onClick={() => this.props.ChangeStat('')}>All</button>
+					</div>
+				</div>
+				<div className="dropdown">
+					<button className="dropbtn">Contributors</button>
+					<div className="dropdown-content">
+						<button onClick={() => this.props.ChangeStat('')}>Top</button>
+						<button onClick={() => this.props.ChangeStat('')}>Roles</button>
+					</div>
+				</div>
+				<div className="dropdown">
+					<button className="dropbtn">Publishers</button>
+					<div className="dropdown-content">
+						<button onClick={() => this.props.ChangeStat('')}>Top Individuals</button>
+						<button onClick={() => this.props.ChangeStat('')}>Top Parents</button>
+						<button onClick={() => this.props.ChangeStat('')}>Locations</button>
+					</div>
+				</div>
 			</div>
 		)
 	}

1f1918ff10546ba59e0297075a3cc6395a267923
Jake Kausler <jakekausler@yahoo.com>
2019-10-10 23:57:38 -0400

    More chart work

--- src/js/StatButtonBar.js
+++ src/js/StatButtonBar.js
@@ -1,38 +1,87 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 class StatButtonBar extends React.Component {
 	constructor() {
 		super()
+		this.state = {
+			active: ''
+		}
+        this.changeStat = this.changeStat.bind(this)
 	}
 
-	//TODO: Change active on click
+	componentDidMount() {
+		this.changeStat($('#generalbycounts')[0])
+	}
+
+	changeStat(target) {
+		console.log(target)
+		if (this.state.active) {
+			$($("#" + this.state.active)[0]).parent().siblings(".dropdown-content").removeClass("active")
+			$($("#" + this.state.active)[0]).removeClass("active")
+		}
+		this.props.ChangeStat(target.id)
+		this.setState({
+			active: target.id
+		})
+		$(target).parent().siblings(".dropdown-content").addClass("active")
+		$(target).addClass("active")
+	}
+
+	//TODO: Dimensions
 	render() {
 		return (
 			<div className="NavBar">
-				<button className="active" onClick={() => this.props.ChangeStat('generalbycounts')}>General</button>
 				<div className="dropdown">
-					<button className="dropbtn">Deweys</button>
+					<button className="dropbtn">General&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
 					<div className="dropdown-content">
-						<button onClick={() => this.props.ChangeStat('')}>Owned</button>
-						<button onClick={() => this.props.ChangeStat('')}>Wishlist</button>
-						<button onClick={() => this.props.ChangeStat('')}>All</button>
+						<a href="#" id='generalbycounts' onClick={(event) => this.changeStat(event.target)}>Count</a>
+						<a href="#" id='generalbypages' onClick={(event) => this.changeStat(event.target)}>Pages</a>
+						<a href="#" id='generalbysize' onClick={(event) => this.changeStat(event.target)}>Size</a>
 					</div>
 				</div>
 				<div className="dropdown">
-					<button className="dropbtn">Contributors</button>
+					<button className="dropbtn">Deweys&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
 					<div className="dropdown-content">
-						<button onClick={() => this.props.ChangeStat('')}>Top</button>
-						<button onClick={() => this.props.ChangeStat('')}>Roles</button>
+						<a href="#" id='deweys' onClick={(event) => this.changeStat(event.target)}>Owned</a>
+						<a href="#" id='deweyswishlist' onClick={(event) => this.changeStat(event.target)}>Wishlist</a>
+						<a href="#" id='deweystotal' onClick={(event) => this.changeStat(event.target)}>All</a>
 					</div>
 				</div>
 				<div className="dropdown">
-					<button className="dropbtn">Publishers</button>
+					<button className="dropbtn">Contributors&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
 					<div className="dropdown-content">
-						<button onClick={() => this.props.ChangeStat('')}>Top Individuals</button>
-						<button onClick={() => this.props.ChangeStat('')}>Top Parents</button>
-						<button onClick={() => this.props.ChangeStat('')}>Locations</button>
+						<a href="#" id='contributorstop' onClick={(event) => this.changeStat(event.target)}>Top</a>
+						<a href="#" id='contributorsperrole' onClick={(event) => this.changeStat(event.target)}>Roles</a>
 					</div>
 				</div>
+				<div className="dropdown">
+					<button className="dropbtn">Publishers&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
+					<div className="dropdown-content">
+						<a href="#" id='publishersbooksperparent' onClick={(event) => this.changeStat(event.target)}>Top Individuals</a>
+						<a href="#" id='publisherstopchildren' onClick={(event) => this.changeStat(event.target)}>Top Parents</a>
+						<a href="#" id='publisherstoplocations' onClick={(event) => this.changeStat(event.target)}>Locations</a>
+					</div>
+				</div>
+				<a href="#" id='series' onClick={(event) => this.changeStat(event.target)}>Series</a>
+				<div className="dropdown">
+					<button className="dropbtn">Languages&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
+					<div className="dropdown-content">
+						<a href="#" id='languagesprimary' onClick={(event) => this.changeStat(event.target)}>Primary</a>
+						<a href="#" id='languagessecondary' onClick={(event) => this.changeStat(event.target)}>Secondary</a>
+						<a href="#" id='languagesoriginal' onClick={(event) => this.changeStat(event.target)}>Original</a>
+					</div>
+				</div>
+				<a href="#" id='formats' onClick={(event) => this.changeStat(event.target)}>Binding</a>
+				<div className="dropdown">
+					<button className="dropbtn">Dates&nbsp;&nbsp;<FontAwesomeIcon icon={faCaretDown} /></button>
+					<div className="dropdown-content">
+						<a href="#" id='datesoriginal' onClick={(event) => this.changeStat(event.target)}>Original</a>
+						<a href="#" id='datespublication' onClick={(event) => this.changeStat(event.target)}>Publication</a>
+					</div>
+				</div>
+				<a href="#" id='' onClick={this.changeStat}>Dimensions</a>
 			</div>
 		)
 	}
--- src/js/Stats.js
+++ src/js/Stats.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { ResponsiveBar } from 'nivo'
+import { ResponsiveBar } from '@nivo/bar'
+import { ResponsivePie } from '@nivo/pie'
 
 import StatButtonBar from './StatButtonBar'
 
@@ -21,9 +22,11 @@ class Stats extends React.Component {
 		fetch("/information/statistics?" + $.param({type: newStat, libraryids: '43'}))
 		.then(res => res.json())
 		.then((data) => {
+			console.log(data, newStat)
 			this.setState({
-				data: data.data.map(function(d) {
+				data: data.data.map(function(d, idx) {
 					return {
+						id: idx + "",
 						value: parseInt(d.val),
 						label: d.label
 					}
@@ -33,23 +36,49 @@ class Stats extends React.Component {
 		.catch(console.log)
 	}
 
-	componentDidMount() {
-		this.changeStat(this.state.current)
-	}
-
+	// TODO: Tooltips with Amount and Percentage
+	// TODO: Publisher City Map
+	// TODO: Publisher Country Map
+	//TODO: Series-Dates
+	//TODO: Dimensions
 	render() {
-        let generalbycountsVisible = this.state.current == 'generalbycounts' ? 'visible' : 'hidden'
+        let barChartVisible = (
+        	this.state.current == 'generalbycounts' ||
+        	this.state.current == 'generalbypages' ||
+        	this.state.current == 'generalbysize'
+        ) ? 'visible' : 'hidden'
+        let pieChartVisible = (
+        	this.state.current == 'deweys' ||
+        	this.state.current == 'deweyswishlist' ||
+        	this.state.current == 'deweystotal' ||
+        	this.state.current == 'contributorstop' ||
+        	this.state.current == 'contributorsperrole' ||
+        	this.state.current == 'publishersbooksperparent' ||
+        	this.state.current == 'publisherstopchildren'
+        ) ? 'visible' : 'hidden'
 
 		return (
 			<div className="stats">
-				<StatButtonBar ChangeStat={this.ChangeStat} />
-				<div className={"barChart " + generalbycountsVisible} style={{width: "100%", height: "750px"}}>
+				<StatButtonBar ChangeStat={this.changeStat} />
+				<div className={"barChart " + barChartVisible} style={{width: "100%", height: "750px"}}>
 					<ResponsiveBar 
 		               data={this.state.data}
 		               indexBy="label"
 		               colorBy="value"
 		               padding={0.3}
 		               margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+		               colors={{"scheme": "set3"}}
+		               labelFormat={(d) => <tspan y={ -12 }>{ d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</tspan>}
+		            />
+	            </div>
+				<div className={"pieChart " + pieChartVisible} style={{width: "100%", height: "750px"}}>
+					<ResponsivePie 
+		               data={this.state.data}
+		               colorBy="value"
+		               radialLabel="label"
+		               padding={0.3}
+		               margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+		               colors={{"scheme": "pastel1"}}
 		            />
 	            </div>
             </div>
